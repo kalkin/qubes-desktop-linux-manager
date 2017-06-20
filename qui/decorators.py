6f5504b5c49faade25b89b92cdc920a4d9d0f6e3
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2017-06-20 14:38:24 +0200

    Rework DomainDecorator
    - Remove unnecessary import
    - DomainDecorator fix label access
    - DomainDecaroter add state decoration method

--- qui/decorators.py
+++ qui/decorators.py
@@ -8,7 +8,7 @@ gi.require_version('Gtk', '3.0')  # isort:skip
 from gi.repository import Gtk  # isort:skip
 
 import qubesadmin
-from qui.models.qubes import LABELS, DomainModel
+from qui.models.qubes import LABELS
 
 
 class PropertiesDecorator():
@@ -36,25 +36,44 @@ class DomainDecorator(PropertiesDecorator):
         super(DomainDecorator, self).__init__(vm, margins)
 
     def name(self):
-        label = Gtk.Label(self.obj.name, xalign=0)
+        label = Gtk.Label(self.obj['name'], xalign=0)
         self.set_margins(label)
         return label
 
     def memory(self) -> Gtk.Label:
-        label = Gtk.Label(str(self.obj.memory) + ' MB', xalign=0)
+        label = Gtk.Label(str(self.obj['memory']) + ' MB', xalign=0)
         self.set_margins(label)
         label.set_sensitive(False)
         return label
 
     def icon(self) -> Gtk.Image:
         ''' Returns a `Gtk.Image` containing the colored lock icon '''
-        label = self.obj.label
+        label_path = self.obj['label']
+        assert label_path in LABELS.children
+        label = LABELS.children[label_path]
         if label is None:
             label = LABELS.BLACK  # pylint: disable=no-member
-        icon_vm = Gtk.IconTheme.get_default().load_icon(label.icon, 22, 0)
+        icon_vm = Gtk.IconTheme.get_default().load_icon(label['icon'], 22, 0)
         icon_img = Gtk.Image.new_from_pixbuf(icon_vm)
         self.set_margins(icon_img)
         return icon_img
+
+    def state(self) -> Gtk.Image:
+        state = self.obj['state']
+        if state == "Halting":
+            icon = Gtk.IconTheme.get_default().load_icon('application-exit', 22, 0)
+        elif state == "Starting":
+            icon = Gtk.IconTheme.get_default().load_icon('system-run', 22, 0)
+        elif state == "Started":
+            icon = Gtk.IconTheme.get_default().load_icon('media-playback-start', 22, 0)
+        elif state == "Halted":
+            icon = Gtk.IconTheme.get_default().load_icon('media-playback-stop', 22, 0)
+        else:
+            icon = Gtk.IconTheme.get_default().load_icon('media-record', 22, 0)
+
+        icon_image = Gtk.Image.new_from_pixbuf(icon)
+        icon_image.set_margin_right(self.margin_right)
+        return icon_image
 
     def netvm(self) -> Gtk.Label:
         netvm = self.obj['netvm']
