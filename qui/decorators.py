c3d5d8b6083c9250f83b84e20305f0e4800fe319
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2017-08-02 11:38:34 +0200

    Add device tray specific decorators

--- qui/decorators.py
+++ qui/decorators.py
@@ -4,11 +4,13 @@ containing helpful representation methods.
 '''
 
 import gi  # isort:skip
+import dbus
 gi.require_version('Gtk', '3.0')  # isort:skip
-from gi.repository import Gtk  # isort:skip
+from gi.repository import Gtk, Pango  # isort:skip
 
 import qubesadmin
 import qui.models.qubes
+from qui.models.qubes import Device, Domain
 
 LABELS = qui.models.qubes.LabelsManager()
 
@@ -43,7 +45,8 @@ class DomainDecorator(PropertiesDecorator):
         return label
 
     def memory(self) -> Gtk.Label:
-        label = Gtk.Label(str(int(self.obj['memory_usage']/1024)) + ' MB', xalign=0)
+        label = Gtk.Label(
+            str(int(self.obj['memory_usage'] / 1024)) + ' MB', xalign=0)
         self.set_margins(label)
         label.set_sensitive(False)
         return label
@@ -68,3 +71,51 @@ class DomainDecorator(PropertiesDecorator):
 
         self.set_margins(label)
         return label
+
+
+def device_hbox(dev: Device) -> Gtk.Box:
+    ''' Returns a :class:`Gtk.Box` containing the device name & icon.. '''
+    if dev['dev_class'] == 'block':
+        icon = 'drive-removable-media'
+    elif dev['dev_class'] == 'mic':
+        icon = 'audio-input-microphone'
+    elif dev['dev_class'] == 'usb':
+        icon = 'generic-usb'
+    else:
+        icon = 'emblem-important'
+    dev_icon = create_icon(icon)
+
+    name = dev.backend_domain['name'] + ":" + dev['ident']
+    if dev["description"] != " ()":
+        name += " - " + dev["description"]
+
+    name_label = Gtk.Label(name, xalign=0)
+    name_label.set_max_width_chars(64)
+    name_label.set_ellipsize(Pango.EllipsizeMode.END)
+
+    hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+    hbox.pack_start(name_label, True, True, 0)
+    hbox.pack_start(dev_icon, False, True, 0)
+    return hbox
+
+def device_domain_hbox(vm: Domain, attached: bool) -> Gtk.Box:
+    hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+
+    # hbox.pack_start(label, True, True, 5)
+
+    if attached:
+        eject_icon = create_icon('media-eject')
+        hbox.pack_start(eject_icon, False, False, 5)
+    else:
+        add_icon = create_icon('list-add')
+        hbox.pack_start(add_icon, False, False, 5)
+
+    name = Gtk.Label(vm['name'], xalign=0)
+    hbox.pack_start(name, True, True, 5)
+    return hbox
+
+
+def create_icon(name: dbus.String) -> Gtk.Image:
+    ''' Create an icon from string '''
+    icon_dev = Gtk.IconTheme.get_default().load_icon(name, 16, 0)
+    return Gtk.Image.new_from_pixbuf(icon_dev)
